7a84305c82643826c63ef7acf7a976c9fac49e2c
Kanstantsin Sharakouski <texet1711@gmail.com>
2021-02-15 02:18:19 +0300

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+    let config = bracketsConfig.map(function(element) {
+        return element.join("")
+    });
+
+    for (let i = 0; i < config.length; i++) {
+        if (str.includes(config[i])) {
+            str = str.replace(config[i], "");
+        }
+    }
+
+    return str == '';
 }
+
